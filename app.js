43dd06942487658b42bc21fc7ed61b12a7a3c52f
Shankar-Sala <shankarsala9995@gmail.com>
2026-02-09 23:25:55 +0530

    error handling

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
-const ejsMate = require("ejs-mate")
+const ejsMate = require("ejs-mate");
 
 // https://mongoosejs.com/
 const MONGO_URL = "mongodb://127.0.0.1:27017/stayora";
@@ -29,8 +29,8 @@ app.use(methodOverride("_method"));
 app.get("/", (req, res) => {
   res.send("Hi, I am Root Page");
 });
-app.engine('ejs', ejsMate)
-app.use(express.static(path.join(__dirname, "/public")))
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 //index route
 app.get("/listings", async (req, res) => {
@@ -51,12 +51,16 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //create Route
-app.post("/listings", async (req, res) => {
+app.post("/listings", async (req, res,next) => {
   // let {title, description, image, price, location, country } = req.body;
-  let listing = req.body.listing;
-  const newListing = new Listing(listing);
-  await newListing.save();
-  res.redirect("/listings");
+
+  try {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  } catch(err) {
+    next(err);
+  }
 });
 
 //edit route
@@ -76,12 +80,11 @@ app.put("/listings/:id", async (req, res) => {
 //Delete Route
 app.delete("/listings/:id", async (req, res) => {
   let { id } = req.params;
- let deleteListings = await Listing.findByIdAndDelete(id)
- console.log(deleteListings);
- 
+  let deleteListings = await Listing.findByIdAndDelete(id);
+  console.log(deleteListings);
+
   res.redirect(`/listings`);
 });
-
 
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
@@ -95,6 +98,11 @@ app.delete("/listings/:id", async (req, res) => {
 //   await sampleListing.save();
 //   console.log("Sample was saved");
 //   res.send("sucessful test");
+// });
+
+//! error handling
+// app.use((err, req, res, next) => {
+//   res.send("Something went wrong");
 // });
 
 app.listen(8080, () => {
